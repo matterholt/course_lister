d47cd1759a0670e66bb636f26f25119ba17c0f8e
matterholt <matterholt.dev@gmail.com>
2023-07-03 22:43:22 -0400

    lesson model

--- courseLister/courseLister_api/models.py
+++ courseLister/courseLister_api/models.py
@@ -9,3 +9,11 @@ class CourseList(models.Model):
     author =  models.CharField(max_length=180, blank=True, default='')
     link =  models.CharField(max_length=180, blank=True, default='')
     subject =  models.CharField(max_length=180, blank=True, default='')
+
+class CourseLesson(models.Model):
+    title = models.CharField(max_length=180, blank=True, default='')
+    chapter_number = models.CharField(max_length=180, blank=True, default='')
+    page_start = models.CharField(max_length=180, blank=True, default='')
+    url_link = models.CharField(max_length=180, blank=True, default='')
+    course_id = models.ForeignKey(CourseList, default=None, on_delete = models.CASCADE, blank = True, null = True)
+
